7e2bf8611a490daa58920159dfcaebbfbcfd1b24
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-08-22 17:49:58 +0500

    User Entity, DbContext & Migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,17 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
