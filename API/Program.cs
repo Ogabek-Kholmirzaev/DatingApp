d2ed54387634999ca08d36508b9cd96fbafd104c
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-08-27 07:36:30 +0500

    Cors & First Client Request

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,12 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(options =>
+    options.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "http://localhost:4200"));
 
 app.UseSwagger();
 app.UseSwaggerUI();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+  
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    });
+  }
 }
