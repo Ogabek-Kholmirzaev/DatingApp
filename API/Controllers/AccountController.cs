c36aa763faabce0ab0484e5fe40768f33d8f0f37
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-09-08 12:41:08 +0500

    Account Controller, Register User Endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register([FromBody] RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Username))
+        {
+            return BadRequest("Username is already taken");
+        }
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = registerDto.Username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+    }
+}
