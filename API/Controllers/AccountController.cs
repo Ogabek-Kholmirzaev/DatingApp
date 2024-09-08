8fe1eef34880cc0890f819d54016e3420be5c6c8
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-09-08 16:01:39 +0500

    Login Endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,6 +33,30 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login([FromBody] LoginDto loginDto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid username");
+        }
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid password");
+            }
+        }
+
+        return user;
+    }
+
     private async Task<bool> UserExists(string username)
     {
         return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
