a3f7bec6a6497a159bf2741adcd6f8305e267903
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-09-24 16:48:14 +0500

    Home Component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,12 +4,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
-import { User } from './_models/user';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NavComponent],
+  imports: [RouterOutlet, CommonModule, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
   title = 'DatingApp';
   users: any;
-  
+
   ngOnInit(): void {
     this.getUsers();
     this.setCurrentUser();
