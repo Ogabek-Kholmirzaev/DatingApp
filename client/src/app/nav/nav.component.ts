b0677ed71a9a5a4b22c6121b01328838490e45c0
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-09-25 17:13:32 +0500

    Angular Routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,23 +2,25 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
   model: any = {};
 
   login() {
     console.log(this.model);
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
+      next: _ => {
+        this.router.navigateByUrl('/members');
       },
       error: error => console.log(error)
     });
@@ -26,5 +28,6 @@ export class NavComponent {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
