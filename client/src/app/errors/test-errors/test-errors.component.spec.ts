5c16b496bc5f9f43bbf0c3394a70a5230b49a469
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-09-26 06:50:59 +0500

    Test Errors Component

---
+++ client/src/app/errors/test-errors/test-errors.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestErrorsComponent } from './test-errors.component';
+
+describe('TestErrorsComponent', () => {
+  let component: TestErrorsComponent;
+  let fixture: ComponentFixture<TestErrorsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestErrorsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TestErrorsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
