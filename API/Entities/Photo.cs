379a1309dcb0aacdd07ed07c66bbaa768401133b
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-09-29 03:57:12 +0500

    Navigation Property & Migraiton

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
 public class Photo
@@ -6,4 +8,8 @@ public class Photo
     public required string Url { get; set; }
     public bool IsMain { get; set; }
     public string? PublicId { get; set; }
+    public int AppUserId { get; set; }
+
+    [ForeignKey(nameof(AppUserId))]
+    public AppUser AppUser { get; set; } = null!;
 }
