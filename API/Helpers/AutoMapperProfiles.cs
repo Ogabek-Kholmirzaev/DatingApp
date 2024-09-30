d3137ae80def467bced0a3d460eb241aeeaf632f
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-09-30 23:12:18 +0500

    AutoMapper Config

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -8,7 +8,9 @@ public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
     {
-        CreateMap<AppUser, MemberDto>();
+        CreateMap<AppUser, MemberDto>()
+            .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+
         CreateMap<Photo, PhotoDto>();
     }
 }
