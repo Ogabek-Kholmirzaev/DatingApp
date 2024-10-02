4ab156ddd03cfd3be42dbd83203f84e7ec2cb5b7
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-10-02 22:21:40 +0500

    Member List Component

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-list',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  private memberService = inject(MembersService);
+  members: Member[] = [];
 
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: members => this.members = members
+    })
+  }
 }
