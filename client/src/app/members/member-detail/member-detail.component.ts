4113607107706e0fa22526d0f01ca114623a084a
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-10-04 23:38:41 +0500

    Member Detail Component

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
+import { TabsModule } from 'ngx-bootstrap/tabs'
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [],
+  imports: [TabsModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit {
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  member?: Member;
 
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+
+    if (!username) {
+      return;
+    }
+
+    this.memberService.getMember(username).subscribe({
+      next: member => this.member = member
+    });
+  }
 }
