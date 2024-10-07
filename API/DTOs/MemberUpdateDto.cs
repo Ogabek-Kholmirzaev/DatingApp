e08448e483a13aca80d465bcaaa22b0e58632af3
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-10-07 17:40:06 +0500

    Fixed Property Naming

--- API/DTOs/MemberUpdateDto.cs
+++ API/DTOs/MemberUpdateDto.cs
@@ -2,7 +2,7 @@ namespace API.DTOs;
 
 public class MemberUpdateDto
 {
-    public string? Instoduction { get; set; }
+    public string? Introduction { get; set; }
     public string? LookingFor { get; set; }
     public string? Interests { get; set; }
     public string? City { get; set; }
