993ecd0e8491b87ebc345ae49232536014740bea
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-06 17:21:26 +0500

    Reactive Form in Register Component

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,29 +1,44 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr'
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService);
   private toastr = inject(ToastrService)
   cancelRegister = output<boolean>();
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initializaForm();
+  }
+
+  initializaForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => this.toastr.error(error.error)
-    })
+    console.log(this.registerForm.value);
+
+    // this.accountService.register(this.model).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => this.toastr.error(error.error)
+    // })
   }
 
   cancel() {
