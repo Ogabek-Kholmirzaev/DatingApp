ff33a6082d6e2a2665c67488b481fd6ffd66479c
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-07 14:39:17 +0500

    Expanding Register User Form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -3,11 +3,12 @@ import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validator
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr'
 import { TextInputComponent } from "../_forms/text-input/text-input.component";
+import { DatePickerComponent } from "../_forms/date-picker/date-picker.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, TextInputComponent],
+  imports: [ReactiveFormsModule, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -18,14 +19,21 @@ export class RegisterComponent implements OnInit {
   cancelRegister = output<boolean>();
   model: any = {};
   registerForm: FormGroup = new FormGroup({});
+  maxDate = new Date();
 
   ngOnInit(): void {
     this.initializaForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18)
   }
 
   initializaForm() {
     this.registerForm = this.fb.group({
+      gender: ['male'],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     });
