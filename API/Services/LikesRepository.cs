5db0ecba629a5388f2809190a3e731c8286c2999
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-28 21:57:07 +0500

    Likes Repository Service

---
+++ API/Services/LikesRepository.cs
@@ -0,0 +1,69 @@
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services;
+
+public class LikesRepository(DataContext context, IMapper mapper) : ILikesRepository
+{
+    public async Task<UserLike?> GetUserLike(int sourceUserId, int targetUserId)
+    {
+        return await context.Likes.FindAsync(sourceUserId, targetUserId);
+    }
+
+    public async Task<IEnumerable<MemberDto>> GetUserLikes(string predicate, int userId)
+    {
+        var likes = context.Likes.AsQueryable();
+
+        switch (predicate)
+        {
+            case "liked":
+                return await likes
+                    .Where(x => x.SourceUserId == userId)
+                    .Select(x => x.TargetUser)
+                    .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                    .ToListAsync();
+            case "likedBy":
+                return await likes
+                    .Where(x => x.TargetUserId == userId)
+                    .Select(x => x.SourceUser)
+                    .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                    .ToListAsync();
+            default:
+                var likeIds = await GetCurrentUserLikeIds(userId);
+
+                return await likes
+                    .Where(x => x.TargetUserId == userId && likeIds.Contains(x.SourceUserId))
+                    .Select(x => x.SourceUser)
+                    .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                    .ToListAsync();
+        }
+    }
+
+    public async Task<IEnumerable<int>> GetCurrentUserLikeIds(int currentUserId)
+    {
+        return await context.Likes
+            .Where(x => x.SourceUserId == currentUserId)
+            .Select(x => x.TargetUserId)
+            .ToListAsync();
+    }
+
+    public void DeleteLike(UserLike like)
+    {
+        context.Likes.Remove(like);
+    }
+
+    public void AddLike(UserLike like)
+    {
+        context.Likes.Add(like);
+    }
+
+    public async Task<bool> SaveChanges()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+}
