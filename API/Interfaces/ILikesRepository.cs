71652913032e7c70009d5aa617d6443f5c115c34
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-28 21:30:17 +0500

    Likes Respository Interface

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,14 @@
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces;
+
+public interface ILikesRepository
+{
+    Task<UserLike?> GetUserLike(int sourceUserId, int targetUserId);
+    Task<IEnumerable<MemberDto>> GetUserLikes(string predicate, int userId);
+    Task<IEnumerable<int>> GetCurrentUserLikeIds(int currentUserId);
+    void DeleteLike(UserLike like);
+    void AddLike(UserLike like);
+    Task<bool> SaveChanges();
+}
