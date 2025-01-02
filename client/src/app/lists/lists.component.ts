d8c2c0130f212b7fe82dbc4b2e4ea2a10b7f64f7
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-01-02 12:34:08 +0500

    Implement Pagination in Likes Service and Update Lists Component for Improved User Experience

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,24 +1,29 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { LikesService } from '../_services/likes.service';
-import { Member } from '../_models/member';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { FormsModule } from '@angular/forms';
 import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [ButtonsModule, FormsModule, MemberCardComponent],
+  imports: [ButtonsModule, FormsModule, MemberCardComponent, PaginationModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent implements OnInit {
-  private likesService = inject(LikesService);
-  members: Member[] = [];
+export class ListsComponent implements OnInit, OnDestroy {
+  likesService = inject(LikesService);
   predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
 
   ngOnInit(): void {
     this.loadLikes();
+  }
+
+  ngOnDestroy(): void {
+    this.likesService.paginatedResult.set(null);
   }
 
   getTitle() {
@@ -33,8 +38,13 @@ export class ListsComponent implements OnInit {
   }
 
   loadLikes() {
-    this.likesService.getLikes(this.predicate).subscribe({
-      next: members => this.members = members
-    });
+    this.likesService.getLikes(this.predicate, this.pageNumber, this.pageSize);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
   }
 }
