b9a62877767c811d84c6b10b0331040433e403a1
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-01-29 22:02:54 +0500

    Implimented GetMessagesForUserAsync Method in MessageRepository

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -9,7 +9,7 @@ public interface IMessageRepository
     void AddMessage(Message message);
     void DeleteMessage(Message message);
     Task<Message?> GetMessageAsync(int id);
-    Task<PagedList<MessageDto>> GetMessagesForUserAsync();
+    Task<PagedList<MessageDto>> GetMessagesForUserAsync(MessageParams @params);
     Task<IEnumerable<MessageDto>> GetMessageThreadAsync(string currentUsername, string recipientUsername);
     Task<bool> SaveAllAsync();
 }
--- API/Services/MessageRepository.cs
+++ API/Services/MessageRepository.cs
@@ -3,10 +3,12 @@ using API.DTOs;
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Services;
 
-public class MessageRepository(DataContext context) : IMessageRepository
+public class MessageRepository(DataContext context, IMapper mapper) : IMessageRepository
 {
     public void AddMessage(Message message)
     {
@@ -23,9 +25,20 @@ public class MessageRepository(DataContext context) : IMessageRepository
         return await context.Messages.FindAsync(id);
     }
 
-    public Task<PagedList<MessageDto>> GetMessagesForUserAsync()
+    public async Task<PagedList<MessageDto>> GetMessagesForUserAsync(MessageParams @params)
     {
-        throw new NotImplementedException();
+        var query = context.Messages.OrderByDescending(x => x.MessageSent).AsQueryable();
+
+        query = @params.Container switch
+        {
+            "Inbox" => query.Where(x => x.Recipient.UserName == @params.Username),
+            "Outbox" => query.Where(x => x.Sender.UserName == @params.Username),
+            _ => query.Where(x => x.Recipient.UserName == @params.Username && x.DateRead == null)
+        };
+
+        var messages = query.ProjectTo<MessageDto>(mapper.ConfigurationProvider);
+
+        return await PagedList<MessageDto>.CreateAsync(messages, @params.PageNumber, @params.PageSize);
     }
 
     public Task<IEnumerable<MessageDto>> GetMessageThreadAsync(string currentUsername, string recipientUsername)
