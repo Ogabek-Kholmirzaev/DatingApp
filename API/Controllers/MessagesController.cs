bd562aaaad6dd78f6954ffe48977dae67476d945
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-01-29 19:16:39 +0500

    Add MessagesController, CreateMessage Endpoint and Updated launchSettings

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,52 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extentions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+
+public class MessagesController(
+    IMessageRepository messageRepository,
+    IUserRepository userRepository,
+    IMapper mapper) : BaseApiController
+{
+    [HttpPost]
+    public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+    {
+        var username = User.GetUsername();
+
+        if (username.Equals(createMessageDto.RecipientUsername, StringComparison.CurrentCultureIgnoreCase))
+        {
+            return BadRequest("You cannot message yourself");
+        }
+
+        var sender = await userRepository.GetUserByUsernameAsync(username);
+        var recipient = await userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+        if (sender == null  || recipient == null)
+        {
+            return BadRequest("Cannot send message at this time");
+        }
+
+        var message = new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = username,
+            RecipientUsername = createMessageDto.RecipientUsername,
+            Content = createMessageDto.Content
+        };
+
+        messageRepository.AddMessage(message);
+
+        if (await messageRepository.SaveAllAsync())
+        {
+            return Ok(mapper.Map<MessageDto>(message));
+        }
+
+        return BadRequest("Failed to save message");
+    }
+}
