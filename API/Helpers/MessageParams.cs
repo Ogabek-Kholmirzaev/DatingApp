55f1f5edf041a4c69bdd36099543fc7b1f688fed
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-01-29 21:48:21 +0500

    MessageParams

---
+++ API/Helpers/MessageParams.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Helpers;
+
+public class MessageParams : PaginationParams
+{
+    public required string Username { get; set; }
+    public string Container { get; set; } = "Unread";
+}
