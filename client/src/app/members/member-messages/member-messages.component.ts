e6c2ac8e27b589deb1ee25547907dd8a4a3d90e4
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-17 16:56:54 +0500

    Activating message tab

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -13,15 +13,8 @@ import { TimeagoModule } from 'ngx-timeago';
 export class MemberMessagesComponent implements OnInit {
   private messageService = inject(MessageService);
   username = input.required<string>();
-  messages: Message[] = [];
+  messages = input.required<Message[]>();
 
   ngOnInit(): void {
-    this.loadMessages();
-  }
-
-  loadMessages() {
-    this.messageService.getMessageThread(this.username()).subscribe({
-      next: messages => this.messages = messages
-    })
   }
 }
