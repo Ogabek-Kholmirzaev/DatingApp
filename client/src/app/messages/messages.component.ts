1d49f32f81e1e3f5d72b76a6d9def8993f86d47c
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-18 00:44:28 +0500

    Improve message display logic and add outbox check in messages component

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -19,6 +19,7 @@ export class MessagesComponent implements OnInit {
   container = 'Inbox';
   pageNumber = 1;
   pageSize = 5;
+  isOutbox = this.container === 'Outbox';
 
   ngOnInit(): void {
     this.loadMessages();
