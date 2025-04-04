80900891d314d558a1a4cd0fc8986c8937749e3f
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-04-04 23:53:56 +0500

    Editing user roles endpoint

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -25,6 +25,40 @@ public class AdminController(UserManager<AppUser> userManager) : BaseApiControll
         return Ok(users);
     }
 
+    [Authorize(Policy = "RequireAdminRole")]
+    [HttpPost("edit-roles/{username}")]
+    public async Task<ActionResult> EditRoles(string username, string roles)
+    {
+        if (string.IsNullOrWhiteSpace(roles))
+        {
+            return BadRequest("You must select at least one role");
+        }
+
+        var selectedRoles = roles.Split(',').ToArray();
+        var user = await userManager.FindByNameAsync(username);
+
+        if (user == null)
+        {
+            return BadRequest("User not found");
+        }
+
+        var userRoles = await userManager.GetRolesAsync(user);
+        var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+        if (!result.Succeeded)
+        {
+            return BadRequest("Failed to add to roles");
+        }
+
+        result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+        if (!result.Succeeded)
+        {
+            return BadRequest("Failed to remove from roles");
+        }
+
+        return Ok(await userManager.GetRolesAsync(user));
+    }
+
     [Authorize(Policy = "ModeratePhotoRole")]
     [HttpGet("photos-to-moderate")]
     public ActionResult GetPhotosForModeration()
